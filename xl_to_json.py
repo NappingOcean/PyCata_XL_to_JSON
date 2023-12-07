3e5661dc155323b8ac7434e608ad18c7b31d39f9
NappingOcean <129575271+NappingOcean@users.noreply.github.com>
2023-12-08 00:32:14 +0900

    Style: 안쓰는 기능들 삭제
    Pandas 가 싫다.

--- xl_to_json.py
+++ xl_to_json.py
@@ -19,8 +19,8 @@
 
 import openpyxl as opyxl
 import json as jo
-import pandas as pd
 
+#import zone
 class Data_Converter:
     # 생성자. 파일의 이름을 변수로서 받는다. 
     # 이 때 클래스 내부에서는 그 이름을 fn으로서 다룬다.
@@ -38,21 +38,7 @@ class Data_Converter:
         # 받은 keys를 / 별로 짤라서 리스트로 저장한다.
         key_serial = keys.split('/')
         return key_serial
-
-    # 복합 딕셔너리에만 사용할 것! 이건 key_serial에만 대응한다.
-    def dix_list_in_k(self, dix: dict, key, val):
-        if isinstance(key, list):
-            if not bool(dix.get(key, False)):
-                dix[key] = []
-            dix[key].append(val)
-
-    #list가 아니라도 list로 만들어주마.
-    def fc_append(self, is_it_list, not_list):
-        if isinstance(is_it_list, list):
-            return (lambda lst:lst.append(not_list))(is_it_list)
-        else:
-            return [is_it_list,not_list]
-
+    
 
     # 해당 셀 아래로 데이터가 있는 셀이 얼마나 연달아 나오는가?
     def col_ser_check(self, ws, row, col):
@@ -64,84 +50,7 @@ class Data_Converter:
                 return ser_num
 
     # vals 는 2차원 리스트로 작성된다.
-    def vals_from_xl(self):
-        xl_data = self.load_xlsx()
-        vals_dict = {}
-        for name in xl_data.sheetnames:
-            xl_sheet = xl_data[name].values
-            vals_list = []
-            # 첫 줄 row 는 key가 있는 곳이다! 
-            # 따라서 rows[0]은 배제한다.
-            for row in list(xl_sheet.rows)[1:]:
-                
-                val_row = []
-                val_id = xl_sheet.cell(row, 1).value
-
-                for col in xl_sheet.columns:
-
-                    val_key = xl_sheet.cell(1, col).value
-                    cel = xl_sheet.cell(row, col).value
-
-                    if cel: # 해당 셀에 값이 있을 경우.
-                        if val_id: # id 존재 시.
-                            if val_key: # key 존재 시.
-                                if (row != xl_sheet.max_row) and not bool(xl_sheet.cell( row + 1, 1 ).value): # 여기가 끝 아님 and 이 아래로 id 없는 값 존재 시.
-                                    no_id_num = self.col_ser_check(xl_sheet, row, col)
-
-                                    v_list_col = [ xl_sheet.cell(list_row, col).value for list_row in xl_sheet.rows[row : row + no_id_num] ]
-                                    # id 없는 모든 값들을 리스트로 흡수함.
-                                    val_row.append(v_list_col)
-                                else: # 다음 값 id 있다
-                                    val_row.append(cel)
-                                
-                                # 리셋
-                                v_list = []
-                                bool_v_2d_list = False
-                        
-                            else: # key 부재 시.
-                                if pre_cel in val_row: 
-                                    val_row.remove(pre_cel) 
-                                    v_list.append(pre_cel)
-                                #pre_cel 이 있든 없든 아래는 진행.
-                                v_list.append(cel)
-                                
-                                #이 아래에 id 없는 값이 있을 시.
-                                if not xl_sheet.cell(row + 1, 1).value:
-                                    # 이 때 cel은 이미 key가 없는 곳의 셀이다
-                                    # 즉 앞의 pre_cel과 v_list를 이미 이루고 있다는 뜻.
-
-                                    no_id_num = self.col_ser_check(xl_sheet, row, col)
-                                    
-                                    # 현재 셀에서 len(v_list) 만큼 왼쪽까지의 셀 값을 list로 만든다
-                                    # 상기의 list 생성 과정을 현재 셀에서 no_id_num 갯수만큼 아래로 내려가며 반복한다
-                                    v_2d_list_col = [[xl_sheet.cell(list_row, list_col).value for list_col in xl_sheet.columns[col+1 -len(v_list): col+1]] for list_row in xl_sheet.columns[row: row + no_id_num]]
-                                    
-                                    bool_v_2d_list = True
-                                
-                                if bool(xl_sheet.cell(1, col+1).value) or (col == xl_sheet.max_column):
-                                    if bool_v_2d_list:
-                                        # 2d list가 되었으면 그걸로 넣는다
-                                        val_row.append(v_2d_list_col)
-                                    else:
-                                        # v_list 를 val 에 추가
-                                        val_row.append(v_list)
-
-                        # 값이 있는 셀은 이전 셀로 만든다.
-                        pre_cel = cel
-                vals_list.append(val_row)
-            vals_dict[name] = vals_list
-        # vals_dict에 저장되는 값은 다음과 같다
-        # vals_dict --> {"name":
-        #                   [
-        #                       [v11, v12, v13, ...]
-        #                       [v21, v22, v23, ...] 
-        #                       ...
-        #                   ]
-        #                }
-        return vals_dict
-
-
-
+    
 
     # 본격적으로 딕셔너리 구축            
     def dix_builder(self, keys: list, vals: list):
@@ -201,127 +110,7 @@ class Data_Converter:
     def json_to_templete(self):
             pass
     
-class File_Loader:
-    def __init__(self, filename):
-        self.fn = filename
-
-    def read_file(self):
-        pdxl = pd.ExcelFile("./"+self.fn, engine= "openpyxl")
-        return pdxl
-    
-    def keys_from_file(self, pdxl:pd.ExcelFile) -> dict: 
-        
-        keys_dict = {}
-
-        for s_name in pdxl.sheet_names:
-            df = pdxl.parse(sheet_name=s_name)
-            list_from_df = list(df.columns.values)
-            for idx in range(len(list_from_df)):
-                if list_from_df[idx].startswith('Unnamed: '):
-                    list_from_df[idx] = None
-                    
-            keys_dict[s_name] = list_from_df
-
-        return keys_dict
-
-    def raw_vals_from_file(self, pdxl:pd.ExcelFile) -> dict: 
-        
-        raw_vals_dict = {}
-
-        for s_name in pdxl.sheet_names:
-            df = pdxl.parse(sheet_name=s_name)
-            list_from_df = df.values.tolist()
-            for lst in list_from_df:
-                for idx in range(len(lst)):
-                    if pd.isna(lst[idx]):
-                        lst[idx] = None
-            raw_vals_dict[s_name] = list_from_df
-
-        return raw_vals_dict
-    
-    # 주어진 row 이하의 col 에서 값은 얼마나 있는가?
-    def amount_checker(self, lst:list, row, col) -> int:
-        ser_num = 1
-        for chk_row in range(row + 1, len(lst)):
-            if lst[chk_row][col] != None and lst[chk_row][0] == None:
-                #col 값이 비어있지 않으면서 id 는 비어있는 경우.
-                ser_num += 1
-            else:
-                break
-        return ser_num
-
-    def vals_pro(self, pdxl:pd.ExcelFile):
-
-        vals_dict = {}
-
-        keys = self.keys_from_file(pdxl)
-        r_vals = self.raw_vals_from_file(pdxl)
-
-        for s_name in pdxl.sheet_names:
-            key_list = keys[s_name]
-            val_list_list = r_vals[s_name]
-            
-            val_dict_in_sht = {}
-            
-            for row in range(len(val_list_list)):
-                val_list = val_list_list[row]
-                val_id = val_list[0]
-                val_dict_in_row = {}
-                tmp_val = []
-                for col in range(len(key_list)):
-                    key_now = key_list[col]
-                    val_now = val_list[col]
-                    if val_id:
-                        if key_now:
-                            val_dict_in_row[key_now] = val_now
-                            key_pre = key_now
-                        elif val_now != None:
-                            tmp_val = val_dict_in_row[list(val_dict_in_row.keys())[-1]]
-                            if isinstance(tmp_val, list):
-                                tmp_val.append(val_now)
-                            else:
-                                tmp_val = [tmp_val, val_now]
-                                val_dict_in_row.update({key_pre:tmp_val})
-                        else: #id 만 있고 키도 값도 없는 자리.
-                            # 거지같은 vitamins_absorb_multi 같으니.
-                            val_up_pre = val_dict_in_row[key_pre]
-                            no_id_val = [[val_list_list[tmp_row][col]] for tmp_row in range(row+1, row + self.amount_checker(val_list_list, row, col))] # [[w#],[x#]]
-                            if not isinstance(val_up_pre, list):
-                                val_up_pre = [val_up_pre, no_id_val] # [v]
-                                val_dict_in_row[key_pre] = [val_up_pre]
-                                # [ v,[[w0],[x0]] ]
-                            else:
-                                # for i in range(len(val_up_pre[-1])):
-                                #     val_up_pre[-1][i].extend(no_id_val[i])
-                                val_dict_in_row[key_pre][0][-1] =[[val_up_pre[0][-1][i][0],no_id_val[i][0]] for i in range(len(val_up_pre[0][-1]))]
-                                    # [ v,[[w0, w1],[x0,x1]] ]
-                        id_pre = val_id
-                    
-                    else:   # 그 자리 id 없음
-                        if val_now != None: # 그 자리 값 있음
-                            if key_now: # 그 자리 키 있음
-                                upper_val = val_dict_in_sht[id_pre][key_now]
-                                if isinstance(upper_val, list):
-                                    # 다음 키가 비어있지 않으면.
-                                    if key_now == key_list[-1] or key_list[key_list.index(key_now)+1]:
-                                        upper_val.append(val_now)
-                                    elif isinstance(upper_val[0], list):
-                                        val_now = [val_list[col+i] for i in range(len(upper_val[0]))]
-                                        upper_val.append(val_now)
-                                    else: # 다음키 빔. 첫요소 리스트 아님.
-                                        val_now = [val_list[col+i] for i in range(len(upper_val))]
-                                        val_dict_in_sht[id_pre][key_now] = [upper_val]
-                                        val_dict_in_sht[id_pre][key_now].append(val_now)
-                                else: # upper_val이 리스트가 아니면.
-                                    val_dict_in_sht[id_pre][key_now] = [upper_val, val_now]
-                                key_pre = key_now
-                # end of for-loop:col
-                val_dict_in_sht.update({val_id:val_dict_in_row})
-            # end of for-loop:row
-            vals_dict[s_name] = list(filter(None, [val_dict_in_sht[key] for key in val_dict_in_sht.keys()]))
-        # end of for-loop:s_name
-        return vals_dict
-    
+ 
 class File_Loader_opyxl:
     def __init__(self, filename):
         self.fn = filename
@@ -449,8 +238,7 @@ class File_Loader_opyxl:
 if __name__ == "__main__":
     
     # Test build
-    main_file = File_Loader("test_XL.xlsx")
-    
+
     main_keys = [
         "id",
         "name/str",
@@ -467,16 +255,7 @@ if __name__ == "__main__":
         [2,6],
         4
     ] 
-
-    # ori_xl = main_file.read_file()
-
-    # main_key_dict = main_file.keys_from_file(ori_xl)
-    # main_val_dict = main_file.raw_vals_from_file(ori_xl)
-    # main_val_adj_dict = main_file.vals_pro(ori_xl)
-
-    # print(main_key_dict)
-    # print(main_val_adj_dict)
-
+    
     # on openpyxl
 
     main_file_px = File_Loader_opyxl("test_XL.xlsx")
